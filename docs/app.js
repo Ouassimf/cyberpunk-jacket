26e4c3abd3ad4791b250e5425de5545bbd1ddef8
Ouassim Fari <ouassim.fari@gmail.com>
2024-10-20 22:24:25 +0200

    updated app

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const connectButton = document.getElementById('connect-button');
     const powerButton = document.getElementById('power-button');
     const tiles = document.querySelectorAll('.tile');
     const brightnessSlider = document.getElementById('brightness');
     const frequencySlider = document.getElementById('frequency');
     const colorPicker = document.getElementById('color-picker');
+
+    // BLE Device Specs
+    var deviceName = 'LED_Jacket';
+    var serviceUUID = '19b10000-e8f2-537e-4f6c-d104768a1214';
+    var mode_characteristicUUID = '19b10001-e8f2-537e-4f6c-d104768a1214';
+    var brightness_characteristicUUID = '19b10002-e8f2-537e-4f6c-d104768a1214';
+    var frequency_characteristicUUID = '19b10003-e8f2-537e-4f6c-d104768a1214';
+    var color_characteristicUUID = '19b10004-e8f2-537e-4f6c-d104768a1214';
+    var power_characteristicUUID = '19b10005-e8f2-537e-4f6c-d104768a1214';
+
+    var bleServer;
+    var bleService;
+    var bleModeCharacteristic;
+    var bleBrightnessCharacteristic;
+    var bleFrequencyCharacteristic;
+    var bleColorCharacteristic;
+    var blePowerCharacteristic;
+
+
 
     let jacketState = {
         power: false,
@@ -12,6 +32,19 @@ document.addEventListener('DOMContentLoaded', function() {
         frequency: frequencySlider.value,
         color: colorPicker.value
     };
+
+    // Connect Button
+    connectButton.addEventListener('click', function() {
+        if (isWebBluetoothEnabled()) {
+            connectToDevice()
+            .then(characteristics => {
+                console.log('Connected to characteristics:', characteristics);
+            })
+            .catch(error => {
+                console.error('Error connecting to device:', error);
+            });
+        }
+    });
 
     // Toggle power state
     powerButton.addEventListener('click', function() {
@@ -49,4 +82,92 @@ document.addEventListener('DOMContentLoaded', function() {
         jacketState.color = colorPicker.value;
         console.log('Color:', jacketState.color);
     });
+
+
+    // Handle characteristic notifications
+    function handleCharacteristicValueChanged(event) {
+        const value = event.target.value;
+        console.log('Received characteristic value:', value);
+    }
+
+    // write characteristic value
+    function writeCharacteristicValue(characteristic, value) {
+        
+    }
+
+    // Function to check if Web Bluetooth is enabled
+    function isWebBluetoothEnabled() {
+        if (!navigator.bluetooth) {
+            console.error('Web Bluetooth API is not available in this browser!');
+            return false;
+        }
+        console.log('Web Bluetooth API is available in this browser!');
+        return true;
+    }
+
+    // Function to handle disconnection
+    function onDisconnected(event) {
+        console.log('Disconnected:', event);
+    }
+
+    // Function to connect to the BLE device
+    function connectToDevice() {
+        return navigator.bluetooth.requestDevice({
+            filters: [{ name: deviceName }],
+            optionalServices: [serviceUUID]
+        })
+        .then(device => {
+            console.log('Connected to device:', device);
+            device.addEventListener('gattserverdisconnected', onDisconnected);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log('Connected to server:', server);
+            bleServer = server;
+            return server.getPrimaryService(serviceUUID);
+        })
+        .then(service => {
+            console.log('Connected to service:', service);
+            bleService = service;
+            return Promise.all([
+                service.getCharacteristic(mode_characteristicUUID),
+                service.getCharacteristic(brightness_characteristicUUID),
+                service.getCharacteristic(frequency_characteristicUUID),
+                service.getCharacteristic(color_characteristicUUID),
+                service.getCharacteristic(power_characteristicUUID)
+            ]);
+        })
+        .then(characteristics => {
+            console.log('Connected to characteristics:', characteristics);
+            bleModeCharacteristic = characteristics[0];
+            bleBrightnessCharacteristic = characteristics[1];
+            bleFrequencyCharacteristic = characteristics[2];
+            bleColorCharacteristic = characteristics[3];
+            blePowerCharacteristic = characteristics[4];
+
+            // Listen for characteristic value changes
+            bleModeCharacteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+            bleBrightnessCharacteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+            bleFrequencyCharacteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+            bleColorCharacteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+            blePowerCharacteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+
+            // Enable notifications
+            bleModeCharacteristic.startNotifications();
+            bleBrightnessCharacteristic.startNotifications();
+            bleFrequencyCharacteristic.startNotifications();
+            bleColorCharacteristic.startNotifications();
+            blePowerCharacteristic.startNotifications();
+
+            console.log('Notifications enabled');
+        
+            return {
+                mode: characteristics[0],
+                brightness: characteristics[1],
+                frequency: characteristics[2],
+                color: characteristics[3],
+                power: characteristics[4]
+            };
+        });
+    }
 });
--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,33 @@
 #include <BLE2902.h>
 
 BLEServer* pServer = NULL;
-BLECharacteristic* pSensorCharacteristic = NULL;
-BLECharacteristic* pLedCharacteristic = NULL;
+BLECharacteristic* pModeCharacteristic = NULL;
+BLECharacteristic* pBrightnessCharacteristic = NULL;
+BLECharacteristic* pFrequencyCharacteristic = NULL;
+BLECharacteristic* pColorCharacteristic = NULL;
+BLECharacteristic* pPowerCharacteristic = NULL;
+
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
+
+// initial values for mode, brightness, frequency, color, power
+uint8_t mode = 3;
+uint8_t brightness = 128;
+uint8_t frequency = 60;
+uint8_t color = 2;
+uint8_t power = 0;
+
 
 const int ledPin = 2; // Use the appropriate GPIO pin for your setup
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID        "19b10000-e8f2-537e-4f6c-d104768a1214"
-#define SENSOR_CHARACTERISTIC_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
-#define LED_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
+#define MODE_CHARACTERISTIC_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
+#define BRIGHTNESS_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
+#define FREQUENCY_CHARACTERISTIC_UUID "19b10003-e8f2-537e-4f6c-d104768a1214"
+#define COLOR_CHARACTERISTIC_UUID "19b10004-e8f2-537e-4f6c-d104768a1214"
+#define POWER_CHARACTERISTIC_UUID "19b10005-e8f2-537e-4f6c-d104768a1214"
 
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -29,30 +43,80 @@ class MyServerCallbacks: public BLEServerCallbacks {
   }
 };
 
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic* pLedCharacteristic) {
-    std::string value = pLedCharacteristic->getValue();
+
+
+class ModeCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pModeCharacteristic) {
+    std::string value = pModeCharacteristic->getValue();
 
     if (value.length() > 0) {
-      Serial.print("Characteristic event, written: ");
-      Serial.println(static_cast<int>(value[0])); // Print the integer value
-
-      int receivedValue = static_cast<int>(value[0]);
-      if (receivedValue == 1) {
-        digitalWrite(ledPin, HIGH);
-      } else {
-        digitalWrite(ledPin, LOW);
-      }
+      Serial.print("Mode Characteristic event, written: ");
+      // Print the value
+      Serial.println(value.c_str());
     }
   }
 };
+
+
+class BrightnessCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pBrightnessCharacteristic) {
+    std::string value = pBrightnessCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.print("Brightness Characteristic event, written: ");
+      // Print the value
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+class FrequencyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pFrequencyCharacteristic) {
+    std::string value = pFrequencyCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.print("Frequency Characteristic event, written: ");
+      // Print the value
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+class ColorCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pColorCharacteristic) {
+    std::string value = pColorCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.print("Color Characteristic event, written: ");
+      // Print the value
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+
+class PowerCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pPowerCharacteristic) {
+    std::string value = pPowerCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.print("Power Characteristic event, written: ");
+      // Print the value
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+
+
+
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 
   // Create the BLE Device
-  BLEDevice::init("ESP32");
+  BLEDevice::init("LED_Jacket");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -62,27 +126,65 @@ void setup() {
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
-  pSensorCharacteristic = pService->createCharacteristic(
-                      SENSOR_CHARACTERISTIC_UUID,
+
+  pModeCharacteristic = pService->createCharacteristic(
+                      MODE_CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_READ   |
                       BLECharacteristic::PROPERTY_WRITE  |
                       BLECharacteristic::PROPERTY_NOTIFY |
                       BLECharacteristic::PROPERTY_INDICATE
                     );
 
-  // Create the ON button Characteristic
-  pLedCharacteristic = pService->createCharacteristic(
-                      LED_CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_WRITE
+  pBrightnessCharacteristic = pService->createCharacteristic(
+                      BRIGHTNESS_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
                     );
 
+  pFrequencyCharacteristic = pService->createCharacteristic(
+                      FREQUENCY_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  pColorCharacteristic = pService->createCharacteristic(
+                      COLOR_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  pPowerCharacteristic = pService->createCharacteristic(
+                      POWER_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+
+
   // Register the callback for the ON button characteristic
-  pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+  pModeCharacteristic->setCallbacks(new ModeCharacteristicCallbacks());
+  pBrightnessCharacteristic->setCallbacks(new BrightnessCharacteristicCallbacks());
+  pFrequencyCharacteristic->setCallbacks(new FrequencyCharacteristicCallbacks());
+  pColorCharacteristic->setCallbacks(new ColorCharacteristicCallbacks());
+  pPowerCharacteristic->setCallbacks(new PowerCharacteristicCallbacks());
+
 
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptor
-  pSensorCharacteristic->addDescriptor(new BLE2902());
-  pLedCharacteristic->addDescriptor(new BLE2902());
+  pModeCharacteristic->addDescriptor(new BLE2902());
+  pBrightnessCharacteristic->addDescriptor(new BLE2902());
+  pFrequencyCharacteristic->addDescriptor(new BLE2902());
+  pColorCharacteristic->addDescriptor(new BLE2902());
+  pPowerCharacteristic->addDescriptor(new BLE2902());
+
 
   // Start the service
   pService->start();
@@ -96,14 +198,28 @@ void setup() {
   Serial.println("Waiting a client connection to notify...");
 }
 
+void notify_all_characteristics() {
+  pModeCharacteristic->notify();
+  pBrightnessCharacteristic->notify();
+  pFrequencyCharacteristic->notify();
+  pColorCharacteristic->notify();
+  pPowerCharacteristic->notify();
+}
+
+void set_all_values() {
+  pModeCharacteristic->setValue(String(mode).c_str());
+  pBrightnessCharacteristic->setValue(String(brightness).c_str());
+  pFrequencyCharacteristic->setValue(String(frequency).c_str());
+  pColorCharacteristic->setValue(String(color).c_str());
+  pPowerCharacteristic->setValue(String(power).c_str());
+}
+
 void loop() {
   // notify changed value
   if (deviceConnected) {
-    pSensorCharacteristic->setValue(String(value).c_str());
-    pSensorCharacteristic->notify();
-    value++;
+    set_all_values();
+    notify_all_characteristics();
     Serial.print("New value notified: ");
-    Serial.println(value);
     delay(3000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
   }
   // disconnecting
